21b39d0384f3438e7cf01167905cbc08a8d4ca1a
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-20 07:38:22 +0530

    Faculty backend

---
+++ src/main/java/com/nestDigital/facultyBackend/FacultyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestDigital.facultyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestDigital/facultyBackend/FacultyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestDigital.facultyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
